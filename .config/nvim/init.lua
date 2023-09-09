06ef5009e9736a1b556e91a9596c465ef384b94a
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2023-09-09 10:11:16 +0400

    prettier width 120, remap horizontal movements

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,42 +1,3 @@
-
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, and understand
-  what your configuration is doing.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -181,7 +142,7 @@ require('lazy').setup({
           html_whitespace_sensitivity = "css",
           -- jsx_bracket_same_line = false,
           jsx_single_quote = false,
-          print_width = 80,
+          print_width = 120,
           prose_wrap = "preserve",
           quote_props = "as-needed",
           semi = true,
@@ -422,7 +383,11 @@ vim.keymap.set('n', 'J', ':m .+1<CR>==', { noremap = true })
 vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv', { noremap = true })
 vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv', { noremap = true })
 
--- vim.keymap.set('n', 'X', 'dd', { noremap = true })
+-- Navigate Horizontally by word
+vim.keymap.set('n', 'H', 'b', { noremap = true })
+vim.keymap.set('n', 'L', 'w', { noremap = true })
+vim.keymap.set('v', 'H', 'b', { noremap = true })
+vim.keymap.set('v', 'L', 'w', { noremap = true })
 
 vim.keymap.set('n', '<leader>sh', '<C-w>h', { noremap = true })
 vim.keymap.set('n', '<leader>sj', '<C-w>j', { noremap = true })
@@ -627,7 +592,7 @@ local servers = {
   jsonls = {},
   vimls = {},
   yamlls = {},
-  -- ccls = {},
+  prismals = {},
 
 
   lua_ls = {
